151c63d3e7dc9d3bc6b472da230a82a2f4bd6d27
Jenni Shearston <jennishearston@jennis-mbp-2.dyn.nyp.org>
2022-12-19 14:08:10 -0500

    first commit - add packages.R

---
+++ packages.R
@@ -0,0 +1,43 @@
+# Packages to Load
+# F31 Google Traffic COVID ITS Analysis
+# Jenni A. Shearston 
+# Updated 11/01/2022
+
+# List of packages to use
+list.of.packages = c('broom',
+                     'corrplot',
+                     'data.table',
+                     'doParallel',
+                     'dlnm',
+                     'egg',
+                     'fst',
+                     'furrr',
+                     'future',
+                     'here',
+                     'Hmisc',
+                     'httr',
+                     'jsonlite',
+                     'lubridate',
+                     'magrittr',
+                     'parallel',
+                     'progress',
+                     'progressr',
+                     'purrr',
+                     'rvest',
+                     'sf',
+                     'splines',
+                     'stringr',
+                     'survival',
+                     'tidycensus',
+                     'tidyverse', 
+                     'wesanderson'
+)
+
+# Check if packages in list are installed, and if not, install 
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Load packages
+lapply(list.of.packages, require, character.only = TRUE)
+
+
