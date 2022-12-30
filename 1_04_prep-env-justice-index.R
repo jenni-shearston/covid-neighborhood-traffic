1ae978f5da39083ada78137f008f606453b1a7dc
Jenni Shearston <jennishearston@jennis-mbp-2.dyn.nyp.org>
2022-12-30 16:23:10 -0500

    finish processing eji data

--- 1_04_prep-env-justice-index.R
+++ 1_04_prep-env-justice-index.R
@@ -44,6 +44,7 @@ eji <- read_csv(paste0(raw_data_path, 'EJI_2022_NY.csv')) %>%
   janitor::clean_names() %>% 
   filter(countyfp == 5 | countyfp == 47 | countyfp == 61 | 
            countyfp == 81 | countyfp == 85) %>% 
+  mutate(geoid = as.character(geoid)) %>% 
   na.omit()
 
 
@@ -57,7 +58,7 @@ eji <- read_csv(paste0(raw_data_path, 'EJI_2022_NY.csv')) %>%
 # 1a Keep only geoid and variables that are estimates rather than percentile
 #    ranks (indicated by a var name starting with ep_ or e_
 eji_nyc <- eji %>% 
-  dplyr::select(geoid, starts_with(c('ep_', 'e_')))
+  dplyr::select(geoid, m_totpop, starts_with(c('ep_', 'e_')))
 
 # 1b Calculate percentile ranks for each estimate and add 'epl' to end of var name
 eji_nyc <- eji_nyc %>% 
@@ -99,76 +100,145 @@ eji_nyc <- eji_nyc %>%
          rpl_eji = percent_rank(spl_eji))
   
 
+####***********************************************************
+#### 2: Recalculate EJI with traffic related vars removed  #### 
+####***********************************************************
+
+# 2a Sum percentile ranks for Environmental Burden Module without traffic
+#    related vars
+#    Notes: Removed PM2.5, diesel particulate matter, high volume roads
+eji_nyc <- eji_nyc %>% 
+  mutate(spl_ebm_sens = # Environmental Burden Module
+           e_ozone_epl + e_totcr_epl + # EBM dom 1
+           e_npl_epl + e_tri_epl + e_tsd_epl + e_rmp_epl + e_coal_epl + e_lead_epl + # EBM dom 2
+           e_park_epl + e_houage_epl + e_wlkind_epl + # EBM dom 3
+           e_rail_epl + e_airprt_epl + # EBM dom 4
+           e_impwtr_epl) # EBM dom 5
+
+# 2b Calculate percentile rank for adjusted EBM score
+eji_nyc <- eji_nyc %>% 
+  mutate(rpl_ebm_sens = percent_rank(spl_ebm_sens))
+
+# 2c Calculate overall EJI using adjusted EBM score
+eji_nyc <- eji_nyc %>% 
+  mutate(spl_eji_sens = rpl_ebm_sens + rpl_svm + rpl_hvm,
+         rpl_eji_sens = percent_rank(spl_eji_sens))
+
+
 ####********************************
-#### 4: Map and review EJI vars #### 
+#### 3: Map and review EJI vars #### 
 ####********************************
 
-# 4a Join eji vars with census tract geo file
-ice_census_chloropleth <- ice_hhincome_race %>% 
-  full_join(tracts, by = c('poly_id' = "geoid"))
+# 3a Join eji vars with census tract geo file
+# 3a.i EJI score recalculated with NYC only tracts in ranking
+eji_nyc_chloropleth <- eji_nyc %>% 
+  full_join(tracts, by = 'geoid')
+# 3a.ii Original EJI score using national tracts in ranking
+eji_chloropleth <- eji %>% 
+  full_join(tracts, by = 'geoid')
 
-# 4b Create chloropleth map -- NH White and Black
-ice_census_chloropleth_map_bw <- ice_census_chloropleth %>% 
-  filter(!is.na(ice_hhincome_bw)) %>% 
-  filter(poly_id %in%gt_fips$poly_id) %>% 
+# 3b Create chloropleth map -- EJI NYC rankings
+# 3b.i EJI score recalculated with NYC only tracts in ranking
+eji_nyc_chloropleth_map <- eji_nyc_chloropleth %>% 
+  filter(geoid %in% gt_fips$poly_id) %>% 
   ggplot() +
-  geom_sf(aes(fill = ice_hhincome_bw, geometry = geometry), lwd = 0) +
-  scale_fill_viridis_c(name = "", 
+  geom_sf(aes(fill = rpl_eji, geometry = geometry), lwd = 0) +
+  scale_fill_viridis_c(name = "EJI Score", 
                        option = "inferno",
-                       breaks = c(-0.38, 0.65),
-                       labels = c("Syst. \nDisadvantaged", 
-                                  "Syst. \nPriveleged")) +
+                       breaks = c(0.05, 1),
+                       labels = c('Low Env Burden', 
+                                  'High Env Burden')) +
   theme_void() +
   theme(panel.grid = element_line(color = "transparent"),
         text = element_text(size = 16))
-ice_census_chloropleth_map_bw
-
-# 4c Create chloropleth map -- NH White and Asian
-ice_census_chloropleth_map_aw <- ice_census_chloropleth %>% 
-  filter(!is.na(ice_hhincome_aw)) %>% 
-  filter(poly_id %in%gt_fips$poly_id) %>% 
+eji_nyc_chloropleth_map
+# 3b.ii Original EJI score using national tracts in ranking
+eji_chloropleth_map <- eji_chloropleth %>% 
+  filter(geoid %in% gt_fips$poly_id) %>% 
   ggplot() +
-  geom_sf(aes(fill = ice_hhincome_aw, geometry = geometry), lwd = 0) +
-  scale_fill_viridis_c(name = "", 
+  geom_sf(aes(fill = rpl_eji, geometry = geometry), lwd = 0) +
+  scale_fill_viridis_c(name = "EJI Score", 
                        option = "inferno",
-                       breaks = c(-0.36, 0.65),
-                       labels = c("Syst. \nDisadvantaged", 
-                                  "Syst. \nPriveleged")) +
+                       breaks = c(0.23, 1),
+                       labels = c('Low Env Burden', 
+                                  'High Env Burden')) +
   theme_void() +
   theme(panel.grid = element_line(color = "transparent"),
         text = element_text(size = 16))
-ice_census_chloropleth_map_aw
+eji_chloropleth_map
+# 3b.iii EJI score recalculated with NYC only tracts in ranking and an 
+#        adjusted EB module to remove traffic items
+eji_nyc_sens_chloropleth_map <- eji_nyc_chloropleth %>% 
+  filter(geoid %in% gt_fips$poly_id) %>% 
+  ggplot() +
+  geom_sf(aes(fill = rpl_eji_sens, geometry = geometry), lwd = 0) +
+  scale_fill_viridis_c(name = "Mod. EJI Score", 
+                       option = "inferno",
+                       breaks = c(0.05, 1),
+                       labels = c('Low Env Burden', 
+                                  'High Env Burden')) +
+  theme_void() +
+  theme(panel.grid = element_line(color = "transparent"),
+        text = element_text(size = 16))
+eji_nyc_sens_chloropleth_map
 
-# 4d Review range and central tendency of ICE vars
-ice_hhincome_race %>% filter(poly_id %in%gt_fips$poly_id) %>% 
-  summarise(min_bw = min(ice_hhincome_bw, na.rm = T),
-            mean_bw = mean(ice_hhincome_bw, na.rm = T),
-            sd_bw = sd(ice_hhincome_bw, na.rm = T),
-            median_bw = median(ice_hhincome_bw, na.rm = T),
-            iqr_bw = IQR(ice_hhincome_bw, na.rm = T),
-            min_aw = min(ice_hhincome_aw, na.rm = T),
-            mean_aw = mean(ice_hhincome_aw, na.rm = T),
-            sd_aw = sd(ice_hhincome_aw, na.rm = T),
-            median_aw = median(ice_hhincome_aw, na.rm = T),
-            iqr_aw = IQR(ice_hhincome_aw, na.rm = T))
+# 3c Review range and central tendency of EJI score
+# 3c.i EJI score recalculated with NYC only tracts in ranking
+eji_nyc %>% filter(geoid %in% gt_fips$poly_id) %>% 
+  summarise(min = min(rpl_eji, na.rm = T),
+            max = max(rpl_eji, na.rm = T),
+            mean = mean(rpl_eji, na.rm = T),
+            sd = sd(rpl_eji, na.rm = T),
+            median = median(rpl_eji, na.rm = T),
+            iqr = IQR(rpl_eji, na.rm = T))
+# 3c.ii Original EJI score using national tracts in ranking
+eji %>% filter(geoid %in% gt_fips$poly_id) %>% 
+  summarise(min = min(rpl_eji, na.rm = T),
+            max = max(rpl_eji, na.rm = T),
+            mean = mean(rpl_eji, na.rm = T),
+            sd = sd(rpl_eji, na.rm = T),
+            median = median(rpl_eji, na.rm = T),
+            iqr = IQR(rpl_eji, na.rm = T))
+# 3c.iii EJI score recalculated with NYC only tracts in ranking and an 
+#        adjusted EB module to remove traffic items
+eji_nyc %>% filter(geoid %in% gt_fips$poly_id) %>% 
+  summarise(min = min(rpl_eji_sens, na.rm = T),
+            max = max(rpl_eji_sens, na.rm = T),
+            mean = mean(rpl_eji_sens, na.rm = T),
+            sd = sd(rpl_eji_sens, na.rm = T),
+            median = median(rpl_eji_sens, na.rm = T),
+            iqr = IQR(rpl_eji_sens, na.rm = T))
 
-# 4e Histograms for ICE vars
-# 4e.i NH White and Black
-ice_hhincome_race %>% filter(poly_id %in%gt_fips$poly_id) %>% 
-  ggplot() + geom_histogram(aes(x = ice_hhincome_bw))
-# 4e.ii NH White and Asian
-ice_hhincome_race %>% filter(poly_id %in%gt_fips$poly_id) %>% 
-  ggplot() + geom_histogram(aes(x = ice_hhincome_aw))
+# 3d Histogram of EJI score
+# 3d.i EJI score recalculated with NYC only tracts in ranking
+eji_nyc %>% filter(geoid %in% gt_fips$poly_id) %>% 
+  ggplot() + geom_histogram(aes(x = rpl_eji))
+# 3d.ii Original EJI score using national tracts in ranking
+eji %>% filter(geoid %in% gt_fips$poly_id) %>% 
+  ggplot() + geom_histogram(aes(x = rpl_eji))
+# 3d.iii EJI score recalculated with NYC only tracts in ranking and an 
+#        adjusted EB module to remove traffic items
+eji_nyc %>% filter(geoid %in% gt_fips$poly_id) %>% 
+  ggplot() + geom_histogram(aes(x = rpl_eji_sens))
 
-# Notes:  
+# Notes: When using the NYC version there is greater variability (as to be expected),
+#        but the mean and median are still very similar. For both scores,
+#        tracts in the South Bronx have the highest env burden scores.
+#        When using the modified EJI for NYC, the min decreases to 0.0005
 
 
 ####**********************
-#### 5: Save out data #### 
+#### 4: Save out data #### 
 ####**********************
 
-# 5a Save out data
+# 4a Keep needed vars
+eji <- eji %>% dplyr::select(geoid, m_totpop, rpl_eji, rpl_ebm, rpl_svm, rpl_hvm)
+eji_nyc <- eji_nyc %>% dplyr::select(geoid, m_totpop, 
+                                     rpl_eji, rpl_ebm, rpl_svm, rpl_hvm,
+                                     rpl_eji_sens, rpl_ebm_sens)
+
+# 4b Save out data
 write_fst(eji, path = paste0(processed_data_path, 'eji.fst')) 
-
+write_fst(eji_nyc, path = paste0(processed_data_path, 'eji_nyc.fst')) 
 
 
